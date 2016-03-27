eced327b2cd7f2c3b8a23079a68deaa6f6409e0e
Ma-Stan <kbrdsmash@gmail.com>
2016-03-28 00:32:07 +0200

    Added saving Q function to .txt files

--- main.cpp
+++ main.cpp
@@ -5,28 +5,43 @@
 #include <vector>
 #include <ctime>
 #include <algorithm>
-
-//std::map <std::pair <state,int>,int> Q_val;
-
-int fn(vector <int> val)
-{
-    return 0;
-
-}
+#include <cstring>
 
 int main()
 {
+    char op[20];
+    srand(1337);
     state cur(vector <pair <int,int> > {{1,2},{1,1}});
-    srand(time(0));
     while (1)
     {
-        int num,rt,shw;
-        scanf("%d%d%d",&num,&rt,&shw);
-        for (int i=0; i<num; i++)
+        scanf("%s",op);
+        if (strcmp(op,"exit")==0)
+            return 0;
+        if (strcmp(op,"play")==0)
         {
-            play_game(rt,shw);
-            if (i%10000==0)
-                printf("DONE %d\n",i);
+            int num,rt,shw;
+            scanf("%d%d%d",&num,&rt,&shw);
+            for (int i=1; i<=num; i++)
+            {
+                play_game(rt,shw);
+                if (i%10000==0)
+                    printf("%d/%d\n",i,num);
+            }
+            printf("Done!\n");
+        }
+        if (strcmp(op,"load")==0)
+        {
+            char file[50];
+            scanf("%s",file);
+            load_from(string(file));
+            printf("Done!\n");
+        }
+        if (strcmp(op,"save")==0)
+        {
+            char file[50];
+            scanf("%s",file);
+            store_to(string(file));
+            printf("Done!\n");
         }
     }
 }
--- q_learn.h
+++ q_learn.h
@@ -1,11 +1,16 @@
 #ifndef _Q_LEARN
 #define _Q_LEARN
+#include "hash_state.h"
 #include "state.h"
 #include <map>
+#include <unordered_map>
 
 void play_game(int rate,int show);
 
+void load_from(string f);
+void store_to(string f);
 
+extern unordered_map <pair <hash_state,int>,int,hash_state> Q_val;
 
 
 #endif // _Q_LEARN
